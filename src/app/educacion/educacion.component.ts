b434804f4a7f7bde484015ea79809aa38948250b
FixBound <santigeasinatra@gmail.com>
2023-01-04 11:50:31 -0300

    implementacion de Expresiones y Pipes

--- src/app/educacion/educacion.component.ts
+++ src/app/educacion/educacion.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent {
 
+export class EducacionComponent {
+  eduObjeto = {
+    otto: {
+      img: "assets/img/Otto Krause.jpeg",
+      name: "ET N°1 Otto Krause / Técnico electrónico ",
+      year: "2015 - 2020",
+      desc: "Paseo Colón 650. Título secundario, formación técnica en Electrónica."
+    },
+    codo: {
+      img: "assets/img/codo a codo.png",
+      name: "Codo a Codo 4.0 ",
+      year: "mar. 2022 - jul. 2022",
+      desc: "Desarollador Full Stack-Java"
+    },
+    argPrograma: {
+      img: "assets/img/argentinaprograma.jpeg",
+      name: "Argentina Programa 2022 ",
+      year: "sept. 2022 - Actualidad",
+      desc: "Desarollador Full Stack(Angular, MySql, JS, CSS) "
+    }
+
+}
 }
