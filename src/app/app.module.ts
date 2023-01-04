434a0264abe3ffcd21baefba7ef71351cb468d51
FixBound <santigeasinatra@gmail.com>
2023-01-04 11:23:37 -0300

    new Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ConocimientosComponent } from './conocimientos/conocimientos.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducacionComponent,
+    ConocimientosComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
